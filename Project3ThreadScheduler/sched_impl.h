64e3a0dd9c633f3aa8f6ffa38106c545528ed7e2
David Moreno <2016moreno@gmail.com>
2021-05-05 14:23:52 -0500

    testing3

--- Project3ThreadScheduler/sched_impl.h
+++ Project3ThreadScheduler/sched_impl.h
@@ -1,5 +1,6 @@
 #ifndef	__SCHED_IMPL__H__
 #define	__SCHED_IMPL__H__
+
 #include <semaphore.h>
 #include <pthread.h>
 #include "list.h"
@@ -7,19 +8,17 @@
 struct thread_info 
 {
 	/*...Fill this in...*/
+	list_elem_t	*queue_elem;
 	struct sched_queue *queue;
-	list_elem_t	*q_e;
-	sem_t *s_cpu; 
-	sem_t *s_ready;
+	sem_t *sem_cpu, *sem_ready;
 };
 
 struct sched_queue 
 {
 	/*...Fill this in...*/
 	list_t *list;
-	sem_t *srelease;
-	sem_t *sadmit;
-	pthread_mutex_t *mutex_un;
+	sem_t *sem_release, *sem_admit;
+	pthread_mutex_t *access_mutex;
 };
 
 #endif /* __SCHED_IMPL__H__ */
