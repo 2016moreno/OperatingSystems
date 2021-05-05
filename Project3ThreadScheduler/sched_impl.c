51acdebf08a166dc79f52e726468fa17c68eb089
David Moreno <2016moreno@gmail.com>
2021-05-05 14:21:30 -0500

    testing2

--- Project3ThreadScheduler/sched_impl.c
+++ Project3ThreadScheduler/sched_impl.c
@@ -1,61 +1,69 @@
-#include "scheduler.h"
-#include "sched_impl.h"
 #include <stdlib.h>
 #include <unistd.h>
-
+#include "scheduler.h"
+#include "sched_impl.h"
 /* Fill in your scheduler implementation code below: */
 
 static void init_thread_info(thread_info_t *info, sched_queue_t *queue)
 {
 	/*...Code goes here...*/
-	info->q_e = (list_elem_t*) malloc(sizeof(list_elem_t));
+	info->queue_elem = (list_elem_t*) malloc(sizeof(list_elem_t));
 	info->queue = queue;
-	info->s_cpu = (sem_t*) malloc(sizeof(sem_t));
-	info->s_ready = (sem_t*) malloc(sizeof(sem_t));
-	sem_init(info->s_cpu, 0, 0);
-	sem_init(info->s_ready, 0, 0);
+
+	info->sem_cpu = (sem_t*) malloc(sizeof(sem_t));
+	info->sem_ready = (sem_t*) malloc(sizeof(sem_t));
+
+	sem_init(info->sem_cpu, 0, 0);
+	sem_init(info->sem_ready, 0, 0);
 }
 
 static void destroy_thread_info(thread_info_t *info)
 {
 	/*...Code goes here...*/
-	free(info->q_e);
+	free(info->queue_elem);
 	info->queue = NULL;
-	sem_destroy(info->s_cpu);
-	sem_destroy(info->s_ready);
-	free(info->s_cpu);
-	free(info->s_ready);
+
+	sem_destroy(info->sem_cpu);
+	sem_destroy(info->sem_ready);
+
+	free(info->sem_cpu);
+	free(info->sem_ready);
 }
 
 /*...More functions go here...*/
 
 static void enter_sched_queue(thread_info_t *info)
 {
-    list_elem_init(info->q_e, info);
-	sem_wait(info->queue->sadmit);
-	pthread_mutex_lock(info->queue->mutex_un);
-	list_insert_tail(info->queue->list, info->q_e);
-	pthread_mutex_unlock(info->queue->mutex_un);
-	sem_post(info->queue->srelease);
+	list_elem_init(info->queue_elem, info);
+
+	sem_wait(info->queue->sem_admit);
+
+	pthread_mutex_lock(info->queue->access_mutex);
+	list_insert_tail(info->queue->list, info->queue_elem);
+	pthread_mutex_unlock(info->queue->access_mutex);
+
+	sem_post(info->queue->sem_release);
 }
 
 static void leave_sched_queue(thread_info_t *info)
 {
-	sem_wait(info->queue->srelease);
-	pthread_mutex_lock(info->queue->mutex_un);
-	list_remove_elem(info->queue->list, info->q_e);
-	pthread_mutex_unlock(info->queue->mutex_un);
-	sem_post(info->queue->sadmit);
+	sem_wait(info->queue->sem_release);
+
+	pthread_mutex_lock(info->queue->access_mutex);
+	list_remove_elem(info->queue->list, info->queue_elem);
+	pthread_mutex_unlock(info->queue->access_mutex);
+
+	sem_post(info->queue->sem_admit);
 }
 
 static void wait_for_cpu(thread_info_t *info)
 {
-    sem_wait(info->s_ready);
+	sem_wait(info->sem_ready);
 }
 
 static void release_cpu(thread_info_t *info)
 {
-    sem_post(info->s_cpu);
+	sem_post(info->sem_cpu);
 }
 
 static void init_sched_queue(sched_queue_t *queue, int queue_size)
@@ -64,13 +72,14 @@ static void init_sched_queue(sched_queue_t *queue, int queue_size)
 	queue->list = (list_t*) malloc(sizeof(list_t));
 	list_init(queue->list);
 
-	queue->sadmit = (sem_t*) malloc(sizeof(sem_t));
-	queue->srelease = (sem_t*) malloc(sizeof(sem_t));
-	queue->mutex_un = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+	queue->sem_admit = (sem_t*) malloc(sizeof(sem_t));
+	queue->sem_release = (sem_t*) malloc(sizeof(sem_t));
 
-	sem_init(queue->sadmit, 0, queue_size);
-	sem_init(queue->srelease, 0, 0);
-	pthread_mutex_init(queue->mutex_un, NULL);
+	queue->access_mutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+
+	sem_init(queue->sem_admit, 0, queue_size);
+	sem_init(queue->sem_release, 0, 0);
+	pthread_mutex_init(queue->access_mutex, NULL);
 }
 
 static void destroy_sched_queue(sched_queue_t *queue)
@@ -84,70 +93,77 @@ static void destroy_sched_queue(sched_queue_t *queue)
 	}
 
 	free(queue->list);
-	sem_destroy(queue->sadmit);
-	sem_destroy(queue->srelease);
-	pthread_mutex_destroy(queue->mutex_un);
 
-	free(queue->sadmit);
-	free(queue->srelease);
-	free(queue->mutex_un);
+	sem_destroy(queue->sem_admit);
+	sem_destroy(queue->sem_release);
+	pthread_mutex_destroy(queue->access_mutex);
+
+	free(queue->sem_admit);
+	free(queue->sem_release);
+	free(queue->access_mutex);
 }
 
 /*...More functions go here...*/
 
 static void wake_up_worker(thread_info_t *info)
 {
-    sem_post(info->s_ready);
+	sem_post(info->sem_ready);
 }
 
-static void wait_for_worker(sched_queue_t *queue)
+void wait_for_worker_fifo (sched_queue_t *queue)
 {
-    if(!(list_get_head(queue->list))) 
-	{
+  	if(!(list_get_head(queue->list))) {
     	return;
 	}
 
-	pthread_mutex_lock(queue->mutex_un);
-  	thread_info_t* qt = (thread_info_t*) (list_get_head(queue->list))->datum;
-	pthread_mutex_unlock(queue->mutex_un);
-	sem_wait(qt->s_cpu);
+	pthread_mutex_lock(queue->access_mutex);
+  	thread_info_t* queue_thread = (thread_info_t*) (list_get_head(queue->list))->datum;
+	pthread_mutex_unlock(queue->access_mutex);
+
+	sem_wait(queue_thread->sem_cpu);
 }
 
-void wait_for_worker_rr(sched_queue_t *queue)
+void wait_for_worker_rr (sched_queue_t *queue)
 {
-	list_elem_t* sched_qh;
+	list_elem_t* sched_queue_head;
 
-	pthread_mutex_lock(queue->mutex_un);
+	pthread_mutex_lock(queue->access_mutex);
 
-	if (!(sched_qh = list_get_head(queue->list)))
+	if (!(sched_queue_head = list_get_head(queue->list)))
 	{
-		pthread_mutex_unlock(queue->mutex_un);
+		pthread_mutex_unlock(queue->access_mutex);
 		return;
 	}
 
-	thread_info_t* queue_thread = (thread_info_t*) sched_qh->datum;
-	list_remove_elem(queue->list, sched_qh);
-	list_insert_tail(queue->list, sched_qh);
-	pthread_mutex_unlock(queue->mutex_un);
-	sem_wait(queue_thread->s_cpu);
+	thread_info_t* queue_thread = (thread_info_t*) sched_queue_head->datum;
+	list_remove_elem(queue->list, sched_queue_head);
+	list_insert_tail(queue->list, sched_queue_head);
+	pthread_mutex_unlock(queue->access_mutex);
+
+	sem_wait(queue_thread->sem_cpu);
 }
 
 static thread_info_t* next_worker(sched_queue_t *queue)
 {
-	list_elem_t* sched_qh;
-	thread_info_t* next_t = NULL;
+	list_elem_t* sched_queue_head;
+	thread_info_t* next_thread = NULL;
 
-	if ((sched_qh = list_get_head(queue->list)))
+	if ((sched_queue_head = list_get_head(queue->list)))
 	{
-		pthread_mutex_lock(queue->mutex_un);
-		next_t = (thread_info_t*) sched_qh->datum;
-		pthread_mutex_unlock(queue->mutex_un);
+		pthread_mutex_lock(queue->access_mutex);
+		next_thread = (thread_info_t*) sched_queue_head->datum;
+		pthread_mutex_unlock(queue->access_mutex);
 	}
-
-	return next_t;
+	return next_thread;
 }
 
-/* Block until at least one worker thread is in the scheduler queue. */
+static void wait_for_queue(sched_queue_t *queue)
+{
+	sem_wait(queue->sem_release);
+	usleep(1000);
+	sem_post(queue->sem_release);
+}
+
 static void wait_for_queue(sched_queue_t *queue)
 {
 	sem_wait(queue->srelease);
